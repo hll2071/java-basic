9088f36184d6854b59719323d860e9ff4fdbf6b7
hsm <hll2071@naver.com>
2024-07-09 14:55:35 +0900

    refactor : for문 조건 유동적으로 변경, ArrayList사용
    일단 어떻게든 ArrayList를 사용해봤다.

--- first_pj/src/Main.java
+++ first_pj/src/Main.java
@@ -1,6 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 class Attacker {
@@ -13,37 +13,40 @@ class Goalkeeper {
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] s = new int[2];
+        int[] miss = new int[2];
+        for(int i = 0; i < miss.length; i++) {
+            miss[i] = 0;
+        }
         String[] balls = new String[2];
-        String keepball = new String();
-        Goalkeeper goalkeeper = new Goalkeeper();
+        String keepball;
         Attacker[] attackers = new Attacker[2];
+        for(int i = 0; i < attackers.length; i++) {
+            attackers[i] = new Attacker();
+        }
         ArrayList<Goalkeeper> goalkeeperlist = new ArrayList<Goalkeeper>();
-        //goalkeeperlist.add(goalkeeper);
-        //ArrayList<Attacker> attackerlist = Arrays.stream(attackers).collect(Collectors.toCollection(ArrayList::new));
+        goalkeeperlist.add(new Goalkeeper());
+        ArrayList<Attacker> attackerlist = Arrays.stream(attackers).collect(Collectors.toCollection(ArrayList::new));
 
-        for(int i=0; i<2; i++) {
-             attackers[i] = new Attacker();
+        for(int i=0; i< attackerlist.size(); i++) {
              System.out.print("공격수 이름을 입력해주세요 : ");
-             attackers[i].name = sc.nextLine();
-            System.out.print("공을 입력해주세요 : ");
+             attackerlist.get(i).name = sc.nextLine();
+             System.out.print("공을 입력해주세요 : ");
              balls[i] = sc.nextLine();
-             attackers[i].ball = balls[i].split(",");
-             //attackerlist.add(attackers[i]);
+             attackerlist.get(i).ball = balls[i].split(",");
         }
         System.out.print("골키퍼의 공을 입력해주세요 : ");
         keepball = sc.nextLine();
 
-        goalkeeper.keep = keepball.split(",");
-        for(int i=0; i<2; i++) {
-            for(String shoot : attackers[i].ball) {
-                for(String goal : goalkeeper.keep) {
-                    if(shoot.contains(goal)) s[i] +=1;
+        goalkeeperlist.get(0).keep = keepball.split(",");
+        for(int i=0; i<attackerlist.size(); i++) {
+            for(String shoot : attackerlist.get(i).ball) {
+                for(String goal : goalkeeperlist.get(0).keep) {
+                    if(shoot.contains(goal)) miss[i] +=1;
                 }
             }
         }
-        if(s[0]>s[1]) System.out.println(attackers[0].name + "님이 승리하셨습니다.");
-        else if (s[0]<s[1]) System.out.println(attackers[1].name + "님이 승리하셨습니다.");
+        if(miss[0]>miss[1]) System.out.println(attackerlist.get(1).name + "님이 승리하셨습니다.");
+        else if (miss[0]<miss[1]) System.out.println(attackerlist.get(0).name + "님이 승리하셨습니다.");
         else System.out.println("무승부입니다.");
     }
 }
