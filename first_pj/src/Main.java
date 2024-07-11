c440b43ba6c90f639d17f57b57dc01359562e434
hsm <hll2071@naver.com>
2024-07-11 17:27:27 +0900

    perf : 첨부터 다시 작성

--- first_pj/src/Main.java
+++ first_pj/src/Main.java
@@ -1,52 +1,30 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
-class Attacker {
-    String name;
-    String[] ball = new String[5];
-}
-class Goalkeeper {
-    String[] keep = new String[4];
-}
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] miss = new int[2];
-        for(int i = 0; i < miss.length; i++) {
-            miss[i] = 0;
-        }
-        String[] balls = new String[2];
-        String keepball;
-        Attacker[] attackers = new Attacker[2];
-        for(int i = 0; i < attackers.length; i++) {
-            attackers[i] = new Attacker();
-        }
-        ArrayList<Goalkeeper> goalkeeperlist = new ArrayList<Goalkeeper>();
-        goalkeeperlist.add(new Goalkeeper());
-        ArrayList<Attacker> attackerlist = Arrays.stream(attackers).collect(Collectors.toCollection(ArrayList::new));
+        int[] miss = new int[]{0,0};
+        ArrayList<String> goalkeeper = new ArrayList<String>();
+        ArrayList<String> attacker1 = new ArrayList<String>();
+        ArrayList<String> attacker2 = new ArrayList<String>();
 
-        for(int i=0; i< attackerlist.size(); i++) {
-             System.out.print("공격수 이름을 입력해주세요 : ");
-             attackerlist.get(i).name = sc.nextLine();
-             System.out.print("공을 입력해주세요 : ");
-             balls[i] = sc.nextLine();
-             attackerlist.get(i).ball = balls[i].split(",");
-        }
-        System.out.print("골키퍼의 공을 입력해주세요 : ");
-        keepball = sc.nextLine();
-
-        goalkeeperlist.get(0).keep = keepball.split(",");
-        for(int i=0; i<attackerlist.size(); i++) {
-            for(String shoot : attackerlist.get(i).ball) {
-                for(String goal : goalkeeperlist.get(0).keep) {
-                    if(shoot.contains(goal)) miss[i] +=1;
-                }
+        System.out.print("attacker1이 찰 공을 입력해주세요 : ");
+        attacker1.add(sc.nextLine());
+        System.out.print("attacker2가 찰 공을 입력해주세요 : ");
+        attacker2.add(sc.nextLine());
+        System.out.print("goalkeeper가 막을 공을 입력해주세요 : ");
+        goalkeeper.add(sc.nextLine());
+        for(String goal : goalkeeper.get(0).split(",")) {
+            for(String shoot : attacker1.get(0).split(",")) {
+                if(shoot.contains(goal)) miss[0] +=1;
+            }
+            for(String shoot : attacker2.get(0).split(",")) {
+                if(shoot.contains(goal)) miss[1] +=1;
             }
         }
-        if(miss[0]>miss[1]) System.out.println(attackerlist.get(1).name + "님이 승리하셨습니다.");
-        else if (miss[0]<miss[1]) System.out.println(attackerlist.get(0).name + "님이 승리하셨습니다.");
+        if(miss[0]<miss[1]) System.out.println("attacker1 님이 승리하셨습니다.");
+        else if (miss[0]>miss[1]) System.out.println("attacker2 님이 승리하셨습니다.");
         else System.out.println("무승부입니다.");
     }
 }
