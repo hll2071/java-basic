5454a12032fca607a76b6d36faed4484fff79c2c
hsm <hll2071@naver.com>
2024-07-12 12:04:14 +0900

    refactor : 코드 간소화

--- first_pj/src/Main.java
+++ first_pj/src/Main.java
@@ -1,27 +1,29 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] miss = new int[]{0,0};
-        ArrayList<String> goalkeeper = new ArrayList<String>();
-        ArrayList<String> attacker1 = new ArrayList<String>();
-        ArrayList<String> attacker2 = new ArrayList<String>();
+        ArrayList<String> goalkeeper;
+        ArrayList<String> attacker1;
+        ArrayList<String> attacker2;
 
         System.out.print("attacker1이 찰 공을 입력해주세요 : ");
-        attacker1.add(sc.nextLine());
+        attacker1 = Arrays.stream(sc.nextLine().split(","))
+                .collect(Collectors.toCollection(ArrayList::new));
         System.out.print("attacker2가 찰 공을 입력해주세요 : ");
-        attacker2.add(sc.nextLine());
+        attacker2 = Arrays.stream(sc.nextLine().split(","))
+                .collect(Collectors.toCollection(ArrayList::new));
         System.out.print("goalkeeper가 막을 공을 입력해주세요 : ");
-        goalkeeper.add(sc.nextLine());
-        for(String goal : goalkeeper.get(0).split(",")) {
-            for(String shoot : attacker1.get(0).split(",")) {
-                if(shoot.contains(goal)) miss[0] +=1;
-            }
-            for(String shoot : attacker2.get(0).split(",")) {
-                if(shoot.contains(goal)) miss[1] +=1;
-            }
+        goalkeeper = Arrays.stream(sc.nextLine().split(","))
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        for(String goal : goalkeeper) {
+            if(attacker1.contains(goal)) miss[0] +=1;
+            if(attacker2.contains(goal)) miss[1] +=1;
         }
         if(miss[0]<miss[1]) System.out.println("attacker1 님이 승리하셨습니다.");
         else if (miss[0]>miss[1]) System.out.println("attacker2 님이 승리하셨습니다.");
